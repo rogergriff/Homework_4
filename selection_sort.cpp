7c287141e5067959a5d56c0eebbb5e478e5998ce
rogergriff <rogergriff121@gmail.com>
2016-03-16 13:35:08 -0700

    Update selection_sort.cpp

--- selection_sort.cpp
+++ selection_sort.cpp
@@ -5,17 +5,6 @@ using std::cin;
 using std::endl;
 using std::vector;
 
-vector<int> selection_sort(vector<int> a);
-/* int main(){
-	vector<int> myVector{ 12, 8, 22, 48, 9, 18, 22};
-	myVector = selection_sort(myVector);
-	cout << "Back in main \n";                              a main used for testing purposes, we don't need to include a main
-	for (int i = 0; i<myVector.size(); ++i)
-		std::cout << myVector[i] << ' ';
-	cout << endl;
-	//system("pause");
-	return 0;
-}   */
 vector<int> selection_sort(vector<int> a){
 	//cout << "There are "<< a.size() << " items in this vector." << endl;  used for testing purposes
 	for (int i = 0; i < a.size(); i++){
