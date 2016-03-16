95a2c219699396b866673f80612088d4b70df38e
rogergriff <rogergriff121@gmail.com>
2016-03-16 13:34:27 -0700

    adding code to main

--- Main.cpp
+++ Main.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include <vector>
+using std::cout;
+using std::endl;
+using std::vector;
+
+vector<int> selection_sort(vector<int> a);
+void print_vector(vector<int>b);
+
+int main(){
+  	vector<int> myVector{ 12, 8, 22, 48, 9, 18, 22};
+	myVector = selection_sort(myVector);
+	print_vector(myVector);
+	//system("pause");
+	return 0;
+}
 
