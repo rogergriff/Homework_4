33306ab0110f02bf4fd23ff281673e344ebdafe0
rogergriff <rogergriff121@gmail.com>
2016-03-16 13:35:58 -0700

    Update printvector.cpp

--- printvector.cpp
+++ printvector.cpp
@@ -4,14 +4,6 @@ using std::cout;
 using std::endl;
 using std::vector;
 
-/*int main(){
-	vector<int> myVector{ 12, 8, 22, 48, 9, 18, 22};
-	cout << "going to print_vector. \n";
-	print_vector(myVector);                              again, including a main for testing purposes.
-	//system("pause");
-	return 0;
-} */
-
 void print_vector(vector<int> b){
 	for (int i = 0; i<b.size(); ++i)
 		cout << b[i] << ' ';
