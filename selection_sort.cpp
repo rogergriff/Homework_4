037d456d4e385240175281f48ab96f7356d8be52
rogergriff <rogergriff121@gmail.com>
2016-03-17 13:12:27 -0700

    Taking out testing things

--- selection_sort.cpp
+++ selection_sort.cpp
@@ -6,7 +6,6 @@ using std::endl;
 using std::vector;
 
 vector<int> selection_sort(vector<int> a){
-	//cout << "There are "<< a.size() << " items in this vector." << endl;  used for testing purposes
 	for (int i = 0; i < a.size(); i++){
 		int smallest;
 		int smallestLocal;
@@ -25,8 +24,5 @@ vector<int> selection_sort(vector<int> a){
 		a[i] = smallest;
 		a[smallestLocal] = temp;
 		}
-/*	for (int i = 0; i<a.size(); ++i)   printing vector for testing purposes.
-	    cout << a[i] << ' ';
-	cout << endl; */
 	return a;
 }
